b6a6ef0d8a5402aa78134b953afce03cc5740d64
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-10-25 21:06:06 +0530

    use-disclose added

---
+++ src/use-disclose/use-disclose.ts
@@ -0,0 +1,35 @@
+import { useCallback, useState } from "react";
+
+export function useDisclose(initialState = false, callbackFns?: { onOpen?: () => void; onClose?: () => void }) {
+    
+    const [opened, setOpened] = useState(initialState);
+    const {onOpen, onClose} = callbackFns || {};
+
+    const open = useCallback(() => {
+         setOpened((isOpened) => {
+            if(!isOpened){
+                onOpen?.();
+                return true;
+            }
+            return isOpened
+         })
+    },[onOpen]);
+
+    const close = useCallback(() => {
+        setOpened((isOpened) => {
+            if(isOpened){
+                onClose?.();
+                return false;
+            }
+            return isOpened;
+        })
+    },[onClose])
+
+    const toggle = useCallback(() => {
+        opened ? close() : open();
+      }, [close, open, opened]);
+
+
+
+    return [opened, {open, close, toggle}] as const;
+}
