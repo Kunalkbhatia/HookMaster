c4ba28ee0d4a409d693fd9e93f3a10835dd68f0f
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-10-27 19:45:27 +0530

    added use-map

---
+++ src/use-map/use-map.ts
@@ -0,0 +1,32 @@
+import { useReducer, useRef } from "react";
+
+const reducer = (value: number) => (value + 1) % 1000000;
+
+export function useRerender(): () => void {
+  const [, update] = useReducer(reducer, 0);
+  return update;
+}
+
+export function useMap<T, V>(initialState?: [T, V][]): Map<T, V> {
+  //  useRef allows you to keep the Map instance (mapRef.current) persist across renders caused by other states. This is important for maintaining the state of the map without disrupting the component lifecycle.
+  const mapRef = useRef(new Map<T, V>(initialState));
+
+  mapRef.current.set = (...keyValuePair) => {
+    Map.prototype.set.apply(mapRef, keyValuePair);
+    useRerender();
+    return mapRef.current;
+  };
+
+  mapRef.current.clear = (...keyValuePair) => {
+    Map.prototype.clear.apply(mapRef, keyValuePair);
+    useRerender();
+  };
+
+  mapRef.current.delete = (...keyValuePair) => {
+    const updatedMap = Map.prototype.delete.apply(keyValuePair, keyValuePair);
+    useRerender();
+    return updatedMap;
+  };
+
+  return mapRef.current;
+}
