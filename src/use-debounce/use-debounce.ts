0e93c8e5db03732cde4a6c205c83b117f18c1506
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-10-27 22:42:13 +0530

    added use-debounce

---
+++ src/use-debounce/use-debounce.ts
@@ -0,0 +1,21 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useDebounce<T = any>(initialValue: T, delay: number) {
+  const [value, setValue] = useState(initialValue);
+  const timerRef = useRef<number | null>(null);
+
+  useEffect(() => clearTimeout(timerRef.current!), []);
+
+  const debouncedSetValue = useCallback((newValue: React.SetStateAction<T>) => {
+    clearTimeout(timerRef.current!);
+
+    timerRef.current = window.setTimeout(() => {
+      setValue(newValue);
+    }, delay);
+  }, []);
+
+  return [value, debouncedSetValue];
+}
+
+
+
