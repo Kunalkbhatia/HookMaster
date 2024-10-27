48e57664370d50a48bdff61f9ea55a7f0819e782
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-10-27 19:50:23 +0530

    added useSet

--- src/use-map/use-map.ts
+++ src/use-map/use-map.ts
@@ -6,21 +6,21 @@ const reducer = (value: number) => (value + 1) % 1000000;
 export function useMap<T, V>(initialState?: [T, V][]): Map<T, V> {
   //  useRef allows you to keep the Map instance (mapRef.current) persist across renders caused by other states. This is important for maintaining the state of the map without disrupting the component lifecycle.
   const mapRef = useRef(new Map<T, V>(initialState));
-
+  const forceRender = useRerender();
   mapRef.current.set = (...keyValuePair) => {
     Map.prototype.set.apply(mapRef, keyValuePair);
-    useRerender();
+    forceRender();
     return mapRef.current;
   };
 
   mapRef.current.clear = (...keyValuePair) => {
     Map.prototype.clear.apply(mapRef, keyValuePair);
-    useRerender();
+    forceRender();
   };
 
   mapRef.current.delete = (...keyValuePair) => {
     const updatedMap = Map.prototype.delete.apply(keyValuePair, keyValuePair);
-    useRerender();
+    forceRender();
     return updatedMap;
   };
 
---
+++ src/use-set/use-set.ts
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import { useRerender } from '../utils/use-rerender';
+
+export function useSet<T>(values?: T[]): Set<T> {
+  const setRef = useRef(new Set(values));
+  const forceRender = useRerender();
+
+  setRef.current.add = (...args) => {
+    const res = Set.prototype.add.apply(setRef.current, args);
+    forceRender();
+
+    return res;
+  };
+
+  setRef.current.clear = (...args) => {
+    Set.prototype.clear.apply(setRef.current, args);
+    forceRender();
+  };
+
+  setRef.current.delete = (...args) => {
+    const res = Set.prototype.delete.apply(setRef.current, args);
+    forceRender();
+
+    return res;
+  };
+
+  return setRef.current;
+}
