6a0e7b2825f6d28e99b0efc9c19aeef323dd3573
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-24 17:01:49 +0530

    Added use-activity

---
+++ src/use-activity/use-activity.ts
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
+
+const DEFAULT_EVENTS: (keyof DocumentEventMap)[] = [
+  'keypress',
+  'mousemove',
+  'touchmove',
+  'click',
+  'scroll',
+];
+const DEFAULT_OPTIONS = {
+  events: DEFAULT_EVENTS,
+  initialState: true,
+};
+
+export function useActivity(
+  timeout: number,
+  options?: Partial<{ events: (keyof DocumentEventMap)[]; initialState: boolean }>
+) {
+  const { events, initialState } = { ...DEFAULT_OPTIONS, ...options };
+  const [activity, setActivity] = useState<boolean>(initialState);
+  const timer = useRef<number>();
+
+  useEffect(() => {
+    const handleEvents = () => {
+      setActivity(false);
+
+      if (timer.current) {
+        window.clearTimeout(timer.current);
+      }
+
+      timer.current = window.setTimeout(() => {
+        setActivity(true);
+      }, timeout);
+    };
+
+    events.forEach((event) => document.addEventListener(event, handleEvents));
+
+    // Start the timer immediately instead of waiting for the first event to happen
+    timer.current = window.setTimeout(() => {
+       setActivity(true);
+    }, timeout);
+
+    return () => {
+      events.forEach((event) => document.removeEventListener(event, handleEvents));
+    };
+  }, [timeout]);
+
+  return activity;
+}
