d914fabe3b7d37ab1333cce0549cc18dda006086
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-27 19:33:11 +0530

    exporting all hooks

--- src/use-get-browser/use-get-browser.ts
+++ src/use-get-browser/use-get-browser.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export function useBrowserName(): string | null {
+export function useGetBrowser(): string | null {
   const [browserName, setBrowserName] = useState<string | null>(null);
 
   useEffect(() => {
